47dd37d17188310d3e68bdbdc8885a543dead9a1
Ymbere <ymbere@gmail.com>
2019-04-03 23:54:55 -0300

    Update new deck
    Update new deck in order to allow the component to submit a new  deck
    to the storage

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,14 +1,56 @@
+//Components
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import { Input, Button } from 'react-native-elements'
+//Redux stuff
+import { connect } from "react-redux";
+//Methods
+import { handleAddDeck } from "../redux/actions/DeckActions";
 
 class NewDeck extends Component {
+    state = {
+        title: "",
+        cards: [],
+        id: 0,
+        redirect: false
+    }
+
+    handleSubmit = () => {
+
+        const { dispatch } = this.props
+        const { title, cards } = this.state
+        const id = Date.now()
+
+        this.setState({
+            id: id
+        })
+
+        dispatch(handleAddDeck({
+            title,
+            id,
+            cards
+        }))
+
+        this.setState({
+            title: ""
+        })
+    }
+
     render() {
         return(
             <View>
-
+                <Text>Digite um nome para o deck</Text>
+                <Input
+                    value={this.state.title}
+                    onChangeText={(title) => this.setState({title})}
+                />
+                <Button
+                    title="Solid button"
+                    onPress={this.handleSubmit}
+                />
             </View>
         )
     }
 }
 
-export default NewDeck
+export default connect()(NewDeck)
