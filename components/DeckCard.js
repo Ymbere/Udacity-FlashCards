d597dbab65294427e70dac5a2d24407beda7f7df
Ymbere <ymbere@gmail.com>
2019-04-03 23:55:24 -0300

    Update deck card
    Update deck card in order to show the deck information

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-
+import { View, Text, TouchableOpacity } from "react-native";
+//Redux stuff
+import { connect } from 'react-redux'
 
 class DeckCard extends Component {
     render() {
+        const { deck } = this.props
+        const { title, cards, id } = deck
         return (
             <View>
-                
+                <TouchableOpacity>
+                    <Text>{title}</Text>
+                </TouchableOpacity>
+                <Text>{cards.length}</Text>
             </View>
         )
     }
 }
 
-export default DeckCard
+const mapStateToProps = ({ decks }, { id }) => {
+    const deck = decks.find(c => c.id === id)
+    return {
+        deck
+    }
+}
+
+export default connect(mapStateToProps)(DeckCard)
