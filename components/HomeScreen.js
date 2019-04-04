be02d01ce12113904a173b1d75f9d752a8ec8575
Ymbere <ymbere@gmail.com>
2019-04-03 23:51:33 -0300

    Add home screen

---
+++ components/HomeScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+//Components
+import DeckList from './DeckList';
+import { Button } from 'react-native-elements';
+import { NavigationActions } from 'react-navigation';
+
+class HomeScreen extends Component {
+    render() {
+        return (
+            <View>
+                <DeckList />
+                <Button
+                    title="Iserir novo deck"
+                    onPress={() => {
+                        this.props.navigation.dispatch(NavigationActions.navigate({ routeName: 'NewDeck' }))
+                    }}
+                />
+            </View>
+        )
+    }
+}
+
+export default HomeScreen
