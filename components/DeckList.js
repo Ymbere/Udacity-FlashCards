d9fe9cd37d5a374e45488f5bcbcbad781a0f72c6
Ymbere <ymbere@gmail.com>
2019-04-03 23:54:06 -0300

    Update deck list
    Update deck list in order to render a flat list to show the cards

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react'
-import { View } from "react-native";
+import { View, TouchableOpacity, StyleSheet, Text, FlatList } from "react-native";
 
 //Redux
 import { connect } from "react-redux";
 import { handleInitialData } from '../redux/actions/Shared';
+//Components
+import DeckCard from './DeckCard';
+import { AppLoading } from "expo";
+import { retriveDecks } from '../utils/API';
+import { receive_deck } from '../redux/actions/DeckActions';
 
 class DeckList extends Component {
+    state = {
+        ready: false
+    }
     componentDidMount() {
-        this.props.dispatch(handleInitialData())
+        const { dispatch } = this.props
+        retriveDecks()
+            .then((decks) => dispatch(receive_deck(decks)))
+            .then(() => this.setState(() => ({
+                ready: true,
+            })))
     }
     render() {
+        const { decks } = this.props
+        const { ready } = this.state
+
+        if (ready === false) {
+            return <AppLoading/>
+        }
+
         return(
             <View>
+                <FlatList
+                    data={decks}
+                    renderItem={({ item }) => (
+                        <DeckCard
+                            id={item.id}
+                        />
+                    )}
+                    keyExtractor={item => item.id.toString()}
+                />
             </View>
         )
     }
