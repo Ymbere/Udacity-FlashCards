44a402daa335f0cf5eac834f8d4de0414052b97c
Ymbere Xavier <ymbere@gmail.com>
2019-04-09 22:37:21 -0300

    Add styles to the deck card

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -9,28 +9,36 @@ class DeckCard extends Component {
         const { deck } = this.props
         const { title, cards, id } = deck
         return (
-            <View style={styles.container}>
-                <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate(
-                        'DeckMainPage',
-                        { deckID: id }
-                    )}
-                >
-                    <Text style={styles.deckTitle}>{title}</Text>
-                </TouchableOpacity>
-                <Text>{cards.length}</Text>
-            </View>
+            <TouchableOpacity
+                style={styles.buttonContainer}
+                onPress={() => this.props.navigation.navigate(
+                    'DeckMainPage',
+                    { deckID: id }
+                )}
+            >
+                <Text style={styles.deckTitle}>{title}</Text>
+                <Text style={styles.cardsNumber}>{cards.length}</Text>
+            </TouchableOpacity>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        alignItems: 'center'
-    },
     deckTitle : {
-        fontSize: 20
+        fontSize: 20,
+        color : '#fff'
+    },
+    buttonContainer: {
+        alignItems: 'center',
+        flex: 1,
+        borderColor: 'black',
+        justifyContent: 'center',
+        backgroundColor: '#34495E',
+        borderBottomColor: '#2C3E50',
+        borderBottomWidth: 1,
+    },
+    cardsNumber : {
+        color: '#bdc3c7'
     }
 })
 
